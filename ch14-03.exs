ee9cbeb943fff7380f58feeb3874042806d26b24
gabriele.lana <gabriele.lana@cleancode.it>
2015-01-15 15:34:07 +0100

    ch14 - 03 description

--- ch14-03.exs
+++ ch14-03.exs
@@ -1,3 +1,9 @@
+# Use spawn_link to start a process, and have that process send a message to the
+# parent and then exit immediately. Meanwhile, sleep for 500 ms in the parent,
+# then receive as many messages as are waiting. Trace what you receive. Does it
+# matter that you weren’t waiting for the notification from the child when it
+# exited?
+
 defmodule Programming.Elixir do
   defmodule CallMeIfYouDie do
     import :timer, only: [sleep: 1]
