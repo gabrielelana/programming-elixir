e69881c29e4929346a22a783ba44a8ed65cdbb4f
gabriele.lana <gabriele.lana@cleancode.it>
2015-01-19 23:18:17 +0100

    ch16 - 02 description

--- ch16-02.exs
+++ ch16-02.exs
@@ -1,3 +1,13 @@
+# You’re going to start creating a server that implements a stack. The call that
+# initializes your stack will pass in a list of the initial stack contents.
+#
+# For now, implement only the pop interface. It’s acceptable for your server to
+# crash if someone tries to pop from an empty stack.
+#
+# For example, if initialized with [5,"cat",9], successive calls to pop will
+# return 5, "cat", and 9.
+
+
 defmodule Programming.Elixir do
   defmodule Stack do
     use GenServer
